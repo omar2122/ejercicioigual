8a337d60670268ebfae4995bf22d011e50bcf00e
Omar García Palazón <omargp87@hotmail.com>
2019-05-07 21:48:42 +0200

    Creación de programa chorra

---
+++ Ejercicioigual1/Ejercicioigual1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicioigual1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bSuma_Click(object sender, EventArgs e)
+        {
+            int num, suma;
+
+            num = 5;
+
+            suma = num + 2;
+
+            MessageBox.Show("La suma da: " + suma);
+        }
+    }
+}
